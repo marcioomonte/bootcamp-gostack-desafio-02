ab38def9d9fd459443191caa436dfc67fb826732
Márcio Monte <marcioomonte@gmail.com>
2019-08-26 22:11:14 -0300

    middleware de autenticação jwt

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.store);
 routes.get('/users', UserController.index); // Listar todos
 routes.post('/users', UserController.store); // Criar
 routes.get('/users/:id', UserController.show); // Buscar
-routes.put('/users/:id', UserController.update); // Editar
 routes.delete('/users/:id', UserController.delete); // Deletar
-
-routes.post('/sessions', SessionController.store);
+routes.use(auth);
+routes.put('/users/:id', UserController.update); // Editar
 
 export default routes;
